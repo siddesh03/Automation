844645beaf767103f3b029e6d6b89dedfe6ca730
siddesh <siddeshsit@gmail.com>
2022-08-31 18:03:50 +0530

    first commit in dev branch

---
+++ Automation.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo $1
+s3name="$1"
+check=`ps -ef |grep apache2 | grep -v grep | wc -l`
+timestamp=$(date +%s)
+if [ "$check" -gt 1 ]
+then 
+echo "apache is running"
+else
+sudo service apache2 restart
+fi 
+cd /var/log/apache2/
+sudo tar -czvf siddeshm-httpd-logs-$timestamp.tar /var/log/apache2/*  && sudo mv siddeshm-httpd-logs-$timestamp.tar /tmp
+cd /tmp
+aws s3 cp siddeshm-httpd-logs-$timestamp.tar s3://$s3name/siddeshm-httpd-logs-$timestamp.tar
+

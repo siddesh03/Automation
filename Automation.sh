14954ab77660f1c9d97d3ea341dd62d6473a17e9
siddesh <siddeshsit@gmail.com>
2022-09-05 17:13:11 +0530

    added to update html in inventory file

--- Automation.sh
+++ Automation.sh
@@ -4,13 +4,22 @@ s3name="$1"
 check=`ps -ef |grep apache2 | grep -v grep | wc -l`
 timestamp=$(date +%s)
 if [ "$check" -gt 1 ]
-then 
+then
 echo "apache is running"
 else
 sudo service apache2 restart
-fi 
+fi
 cd /var/log/apache2/
 sudo tar -czvf siddeshm-httpd-logs-$timestamp.tar /var/log/apache2/*  && sudo mv siddeshm-httpd-logs-$timestamp.tar /tmp
 cd /tmp
 aws s3 cp siddeshm-httpd-logs-$timestamp.tar s3://$s3name/siddeshm-httpd-logs-$timestamp.tar
-# till verison 1
+cd /var/www/html
+file_name1='siddeshm-httpd-logs-'
+file_name2="$timestamp.tar"
+file_name="${file_name1}${file_name2}"
+file_check="/tmp/${file_name}"
+FILESIZE=$(stat -c%s "$file_check")
+
+file="<tr><td>'$file_name'</td>&nbsp;&nbsp<td>$timestamp</td> &nbsp;&nbsp<td>tar</td>&nbsp;&nbsp<td>$FILESIZE</td></tr>"
+sed -i "/<\/table>/i'$file'" inventory.html
+
